6d4667bd5a5c04670c3a88c05c1f61b457115672
Ryan <ryanzhke@gmail.com>
2022-11-11 09:32:51 -0500

    Add new portfolio template

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,6 @@ if (navClose) {
 		navMenu.classList.remove("show-menu");
 	});
 }
-
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll(".nav__link");
 
@@ -28,83 +27,6 @@ function linkAction() {
 	navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-/*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName("skills__content"),
-	skillsHeader = document.querySelectorAll(".skills__header");
-
-function toggleSkills() {
-	let itemClass = this.parentNode.className;
-
-	for (i = 0; i < skillsContent.length; i++) {
-		skillsContent[i].className = "skills__content skills__close";
-	}
-	if (itemClass === "skills__content skills__close") {
-		this.parentNode.className = "skills__content skills__open";
-	}
-}
-
-skillsHeader.forEach((el) => {
-	el.addEventListener("click", toggleSkills);
-});
-
-/*==================== QUALIFICATION TABS ====================*/
-const tabs = document.querySelectorAll("[data-target]"),
-	tabContents = document.querySelectorAll("[data-content]");
-
-tabs.forEach((tab) => {
-	tab.addEventListener("click", () => {
-		const target = document.querySelector(tab.dataset.target);
-
-		tabContents.forEach((tabContent) => {
-			tabContent.classList.remove("qualification__active");
-		});
-		target.classList.add("qualification__active");
-
-		tabs.forEach((tab) => {
-			tab.classList.remove("qualification__active");
-		});
-		tab.classList.add("qualification__active");
-	});
-});
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".mySwiper", {
-	cssMode: true,
-	loop: true,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-});
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll("section[id]");
-
-function scrollActive() {
-	const scrollY = window.pageYOffset;
-
-	sections.forEach((current) => {
-		const sectionHeight = current.offsetHeight;
-		const sectionTop = current.offsetTop - 50;
-		sectionId = current.getAttribute("id");
-
-		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-			document
-				.querySelector(".nav__menu a[href*=" + sectionId + "]")
-				.classList.add("active-link");
-		} else {
-			document
-				.querySelector(".nav__menu a[href*=" + sectionId + "]")
-				.classList.remove("active-link");
-		}
-	});
-}
-window.addEventListener("scroll", scrollActive);
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
@@ -124,38 +46,12 @@ function scrollUp() {
 }
 window.addEventListener("scroll", scrollUp);
 
-/*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "uil-sun";
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
-
-// Obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () =>
-	document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () =>
-	themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
-
-// Validate if the user previously chose a topic
-if (selectedTheme) {
-	// If the validation is fulfilled, ask what the issue was to know if we activated or deactivated the dark
-	document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-		darkTheme
-	);
-	themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](
-		iconTheme
-	);
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener("click", () => {
-	// Add or remove the dark / icon theme
-	document.body.classList.toggle(darkTheme);
-	themeButton.classList.toggle(iconTheme);
-	// Save the theme and the current icon that the user chose
-	localStorage.setItem("selected-theme", getCurrentTheme());
-	localStorage.setItem("selected-icon", getCurrentIcon());
-});
+/*==================== DISABLE RIGHT CLICK ON IMAGE ====================*/
+window.addEventListener(
+	"contextmenu",
+	function (ev) {
+		ev.preventDefault();
+		return false;
+	},
+	false
+);
